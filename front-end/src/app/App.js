691199265535d202ec826f682fb199bcaf5d3eff
Isuru Lakmal <92336775+IsuruGithu@users.noreply.github.com>
2022-10-06 16:05:13 +0530

    Delete front-end directory

--- front-end/src/app/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import HomePage from "../view/Home/home";
-function App() {
-  return (
-    <HomePage/>
-  );
-}
-
-export default App;
--- front-end/src/view/Home/style.js
+++
@@ -1,5 +0,0 @@
-/**
- * @author : Isuru Lakmal G K A
- * @since : 0.1.0
- **/
-    
